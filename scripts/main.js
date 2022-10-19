7ebace152f3d1727651880a8c49d18a936a6b4f4
amirsefidgar82 <106591238+amirsefidgar82@users.noreply.github.com>
2022-10-19 10:29:32 +0330

    syntax correction

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let btn = document.getElementById("submit");
 function username_check() {
-  var username = document.getElementById("username").value;
-  var username_error = document.getElementById("username_error");
+  let username = document.getElementById("username").value;
+  let username_error = document.getElementById("username_error");
   try {
     if (username.length === 0) throw "نام کاربری نمیتواند خالی بماند";
     if (username.length !== 0) {
@@ -9,14 +9,14 @@ function username_check() {
     }
   } catch (errr) {
     username_error.style.display = "block";
-    username_error.innerHTML = errr;
+    username_error.innerText = errr;
     btn.setAttribute("disabled", "");
   }
   btn_status();
 }
 function password_check() {
-  var password = document.getElementById("password").value;
-  var password_error = document.getElementById("password_error");
+  let password = document.getElementById("password").value;
+  let password_error = document.getElementById("password_error");
   try {
     if (password.length === 0) throw "پسورد نمیتواند خالی بماند";
     if (password.length < 6)
@@ -26,7 +26,7 @@ function password_check() {
     }
   } catch (err) {
     password_error.style.display = "block";
-    password_error.innerHTML = err;
+    password_error.innerText = err;
     btn.setAttribute("disabled", "");
   }
   btn_status();
